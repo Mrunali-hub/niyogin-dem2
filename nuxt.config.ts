62a21a3a355bcfc2183937b5b1f00b061e8be087
Mrunali Melekar <melekarmrunali@gmail.com>
2023-11-24 11:01:14 +0530

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import {resolve} from "path";
+
+export default defineNuxtConfig({
+  alias:{
+    "@": resolve(__dirname, "/")
+  },
+  css:["~/assests/main.scss"],
+
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  generate: {
+    routes: [
+      '/contact',  // Add your routes here
+    ],
+  },
+  
+})
